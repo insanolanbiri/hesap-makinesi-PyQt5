d6f9d7b3498e4d0a2b77962ee09c32431e8446b4
insanolanbiri <erenakgun2007@hotmail.com>
2022-01-16 13:29:16 +0300

    update readme on source

--- hesap.py
+++ hesap.py
@@ -11,7 +11,7 @@ Hesap Makinesi by Eren
 mükemmel bir hesap makinesi.
 ----------------------------
 
-![ekran görüntüsü](https://user-images.githubusercontent.com/75967441/149652980-ed5a0bf2-c85b-4d15-99e0-71eb4a429ca4.png "Hesap Makinesi by Eren")
+![ekran görüntüsü ubuntu](https://user-images.githubusercontent.com/75967441/149655076-a4c6302b-ba6c-4365-a6a1-764fda2d2a35.png "Hesap Makinesi by Eren (Ubuntu/GNOME3)") ![ekran görüntüsü windows](https://user-images.githubusercontent.com/75967441/149655847-7183f87a-fbed-4bc4-8833-9054cf3c838a.png "Hesap Makinesi by Eren (Windows)")
 
 bazılarının inanmak istemeyeceğini bilsem de bunu ben yaptım;
 internetten vs. çalmadan.
@@ -24,7 +24,9 @@ iddialıysanız internetten aynısını bulmayı deneyebilirsiniz.
 şimdi bana kesin gelip bu çalışmıyor diyeceksiniz;
 bi zahmet PyQt5'i kurun, beni delirtmeyin: ```pip install PyQt5```
 
-(eğer pip3 ile indirmede sıkıntı yaşıyorsanız, [şunu](https://sourceforge.net/projects/pyqt/files/PyQt5/ "PyQt5'in sourceforge reposu") da deneyebilirsiniz (ben denemedim))
+eğer pip3 ile indirmede sıkıntı yaşıyorsanız, [şunu](https://sourceforge.net/projects/pyqt/files/PyQt5/ "PyQt5'in sourceforge reposu") da deneyebilirsiniz (ben denemedim)
+
+eğer o da karışık gelirse benim windows için derlediğim [binaryleri](https://github.com/insanolanbiri/hesap-makinesi-PyQt5/releases/latest "binaryler") de kullanabilirsiniz.
 """
 
 def asalcarpan(sayi):
