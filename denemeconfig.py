38a4486a84b839193c2d939478912d1ae134d815
insanolanbiri <erenakgun2007@hotmail.com>
2022-02-01 20:01:38 +0300

    ne eklediğimi hatırlamıyorum

--- denemeconfig.py
+++ denemeconfig.py
@@ -1,6 +1,7 @@
 # deneme ve inceleme için değişkenler dosyası.
+# insanolanbiri/deneme/denemeconfig.py dosyasının düzenlenmiş sürümü.
 url="https://aydinyayinlari.karnemiz.com/?pg=ogrgiris" # giriş yapılacak url
-# not: scriptin çalışması için deneme sonuç urlsinin `orbim` adındaki
+# not: scriptin çalışması için deneme sonuç urlsi `orbim` adındaki
 #      bir şirketin yaptığı arayüzü kullanmalı.
 puanbosluk=6 # aq sitesinde puan yazısı "Puanınız_vs: intPuan" olarak yazıldığı için bunu ekliyoruz.
 i_sinif="9.Sınıf" # sınıf seviyesi, boşluk olmadan: "9.Sınıf" gibi.
@@ -8,14 +9,8 @@ i_sehir="EDİRNE" # şehir, tamamı büyük harf: "EDİRNE" gibi.
 i_kurum="Edirne Süleyman Demirel" # okulun adı, siteye yazıldığında listede ilk sonuç olarak görünmeli.
 
 
-
-
-# not: bu isimleri başka bir yerden bulup güncellemeniz lazım.
-sinif9a=[
-]
-
-sinif9b=[
-]
-
-sinif9c=[
-]
+# not: sınıf listelerini başka bir yerden bulup güncellemeniz lazım.
+#      kendimi test amaçlı kullanabilirsiniz :)
+sinif9a=[["Eren Akgün",7]]
+sinif9b=[["Eren Akgün",7]]
+sinif9c=[["Eren Akgün",7]]
